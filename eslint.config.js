ee75d449dad8bf10775b8f53869a585d084f2e23
kola kolade <kolakolade@gmail.com>
2024-06-18 21:38:40 +0100

    Configure the project to run pretter and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,30 @@
-import config from "eslint-config-standard";
-
+import config from 'eslint-config-standard'
+import prettier from 'eslint-plugin-prettier'
+import globals from 'globals'
 
 export default [
   ...[].concat(config),
-];
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      prettier,
+    },
+    languageOptions: {
+      parserOptions: {
+        context: {
+          ecmaVersion: 2021, // specify the ECMAScript version, adjust as needed
+          sourceType: 'module', // specify module type, can be 'script' or 'module'
+        },
+        globals: {
+          ...globals.browser,
+          myCustomGlobal: 'readonly',
+        },
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      // Add any other ESLint rules you want to override here
+    },
+  },
+  { ignores: ['.config/*', '**/.config/'] },
+]
